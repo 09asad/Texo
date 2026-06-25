00aee77729fc2cd97c4dcc8422a85f2a71807cc5
Asad Khan <asadkhan258011@gmail.com>
2026-06-25 16:30:08 +0530

    Add database connection and user/message models; update dependencies

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,26 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
 
-const app = express();
-const PORT = process.env.PORT;
+import { connectDB } from "./lib/db.js";
 
-app.listen(PORT, () => console.log("Server is listening at the PORT:",PORT));
+import { clerkMiddleware } from '@clerk/express';
+
+const app = express();
+
+const PORT = process.env.PORT;
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
+app.use(express.json());
+app.use(cors({origin: FRONTEND_URL, credentials: true}));
+
+app.use(clerkMiddleware());
+ 
+app.get("/health", (req,res) => {
+    res.status(200).json({ok : true});
+});
+
+app.listen(PORT, () => {    
+    connectDB();
+    console.log("Server is listening at the PORT:",PORT)
+});
