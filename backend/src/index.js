f25ebc658ba26fd437257457a85ceb5ed74709fd
Asad Khan <asadkhan258011@gmail.com>
2026-06-29 08:47:28 +0530

    Implement Clerk authentication and update frontend structure; add static file serving

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import cors from "cors";
+
 import "dotenv/config";
+
+import fs from "fs";
+import path from "path";
 
 import { connectDB } from "./lib/db.js";
 
@@ -11,14 +15,23 @@ const app = express();
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
+const publicDir = path.join(process.cwd(), "public");
+
 app.use(express.json());
 app.use(cors({origin: FRONTEND_URL, credentials: true}));
-
 app.use(clerkMiddleware());
- 
+
 app.get("/health", (req,res) => {
     res.status(200).json({ok : true});
 });
+
+if(fs.existsSync(publicDir)){           /* does the "public" folder exists? (only true in production) */
+    app.use(express.static(publicDir))   /*serve the built react files */
+
+    app.get("/{*any}", (req,res,next) => {             /* any other URL -> sends back index.html */
+        res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+    });
+}
 
 app.listen(PORT, () => {    
     connectDB();
